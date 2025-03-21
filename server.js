47f549707941ad5f27645c50f7adf710244a9ecd
shanmukha <shanuthanudevkam@gmail.com>
2025-03-21 08:25:38 +0530

    Included cors

--- server.js
+++ server.js
@@ -71,9 +71,11 @@ const PORT = process.env.PORT || 3030;
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: ['https://think-foody.vercel.app', 'http://localhost:3030']
-}));
+// app.use(cors({
+//   origin: ['https://think-foody.vercel.app', 'http://localhost:3030']
+// }));
+
+app.use(cors());
 
 // Static file serving
 app.use(express.static(__dirname));
